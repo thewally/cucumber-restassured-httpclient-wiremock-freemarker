3c1661196c979c7c72b9f6a1ede82ba9aa7d801d
Arjen van der Wal <arjen.van.der.wal@rabobank.nl>
2017-04-11 20:30:47 +0200

    Added TemplateHandler

--- src/main/java/nl/thewally/freemarker/TemplateHandler.java
+++ src/main/java/nl/thewally/freemarker/TemplateHandler.java
@@ -1,7 +1,73 @@
 package nl.thewally.freemarker;
 
-/**
- * Created by arjen on 10-4-17.
- */
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import freemarker.template.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
 public class TemplateHandler {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TemplateHandler.class);
+    private static final String TEMPLATE_PATH = "/src/test/resources/templates/";
+
+    private final Configuration cfg = new Configuration();
+    private final Map<String, Object> data = new HashMap<>();
+
+    private Template template;
+
+    public TemplateHandler() {
+    }
+
+    public TemplateHandler(String template) throws IOException {
+        this.template = cfg.getTemplate(TEMPLATE_PATH + template);
+    }
+
+    public void setTemplate(String template) throws IOException {
+        this.template = cfg.getTemplate(TEMPLATE_PATH + template);
+    }
+
+    public void setValue(String variable, String value) {
+        data.put(variable, value);
+    }
+
+    public void setValue(String variable, int value) {
+        data.put(variable, value);
+    }
+
+    public void setValue(String variable, List value) {
+        data.put(variable, value);
+    }
+
+    public void setValue(String variable, Map value) {
+        data.put(variable, value);
+    }
+
+    public String getOutput() throws TemplateException, IOException {
+        try {
+            Writer result = new StringWriter();
+            template.process(data, result);
+            return result.toString();
+        } catch (IOException e) {
+            LOG.error("Cannot generate output: \n{}", e);
+        } catch (TemplateException e) {
+            LOG.error("Cannot generate template output: \n{}", e);
+        }
+        return null;
+    }
+
 }
+
