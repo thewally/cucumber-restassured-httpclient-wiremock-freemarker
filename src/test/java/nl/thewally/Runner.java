75c1fef06cc158ec63f7d9840b13ad5a7b501022
arjen <arjen@detesters.nl>
2017-11-23 13:21:55 +0100

    Removed unused proeprtyfile

--- src/test/java/nl/thewally/Runner.java
+++ src/test/java/nl/thewally/Runner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         monochrome = true,
-        plugin = {"pretty", "json:target/cucumber"},
+        plugin = {"pretty", "html:target/cucumber"},
         dryRun = false,
         glue = {"nl.thewally.stepdefs"},
         features = {"src/test/resources/features"}
